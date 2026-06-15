b191301e85eb877c3a627dcb9232458658338b9c
IT26102145 <it26102145@my.sliit.lk>
2026-06-15 19:37:05 +0530

    Adding IT26102145Lab2Q1.java to Q1

---
+++ Q1/IT26102145Lab2Q1.java
@@ -0,0 +1,13 @@
+public class IT26102145Lab2Q1{
+	
+	public static void main (String []args){
+		
+		double perimeter = 100;
+		double length = (perimeter*2)/7;
+		double width = length*(3.0/4);
+		
+		System.out.println("length of the fence: " +length);
+		System.out.println("width of the fence: " +width);
+		
+	}
+}
