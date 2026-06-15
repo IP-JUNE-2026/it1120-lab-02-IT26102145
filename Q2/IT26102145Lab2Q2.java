7b8be5c37d5eaa9e12d84a336868831dd79567ae
IT26102145 <it26102145@my.sliit.lk>
2026-06-15 19:38:11 +0530

    Adding IT26102145Lab2Q2.java to Q2

---
+++ Q2/IT26102145Lab2Q2.java
@@ -0,0 +1,12 @@
+public class IT26102145Lab2Q2{
+	
+	public static void main (String []args){
+		
+		double length = 10;
+		double perimeter = 4*length;
+		double radius = perimeter /(2.0*3.14);
+		
+		System.out.println("Radius of the circular fence: " +radius);
+		
+	}
+}
